60695c61dcecc88e82712f93b9dd1c715146c6cb
AnnaxGrace <anna.grace.conover@gmail.com>
2020-06-23 16:39:52 -0500

    Updates

---
+++ precache-manifest.2667fc7730a22027f3a5e5f66769dc82.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "97cf282109e46ee1abcbd8d25a27459f",
+    "url": "/Employee-Directory/index.html"
+  },
+  {
+    "revision": "d0c6006742d78c995e4f",
+    "url": "/Employee-Directory/static/css/main.5958e323.chunk.css"
+  },
+  {
+    "revision": "594a919bb2c02d6ad0e8",
+    "url": "/Employee-Directory/static/js/2.22efb6e2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Employee-Directory/static/js/2.22efb6e2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d0c6006742d78c995e4f",
+    "url": "/Employee-Directory/static/js/main.c6974d94.chunk.js"
+  },
+  {
+    "revision": "21ab4dbde2f8e2bfdf72",
+    "url": "/Employee-Directory/static/js/runtime-main.965eecad.js"
+  }
+]);
